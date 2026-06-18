6f74c3d2ed4d863459bb717526a0e3aa936d1da1
torstpen <torstpen@gmail.com>
2026-06-18 15:02:52 +0200

    Create __init__.py

---
+++ viewport_component/__init__.py
@@ -0,0 +1,6 @@
+import streamlit.components.v1 as components
+
+viewport_height_component = components.declare_component(
+    "viewport_height_component",
+    path="viewport_component/frontend",
+)
