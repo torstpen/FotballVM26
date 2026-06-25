1eb149b84e4297332f19160d64b27f26c9096838
torstpen <torstpen@gmail.com>
2026-06-25 21:26:25 +0200

    Update App.py

--- App.py
+++ App.py
@@ -475,7 +475,7 @@ if not label_df.empty:
     fig.add_trace(
         go.Scatter(
             x=[now] * len(label_df),
-            y=label_df["Poeng"] + 0.5,
+            y=label_df["Poeng"] + 1,
             mode="text",
             text=label_df["Deltaker"],
             textposition="middle left",
